12418606b232783a170db391285d98e749d7644e
JinJis <david.jeong0724@gmail.com>
2022-09-12 23:30:50 +0900

    Add user/my endpoint

--- heythere/apps/user/urls.py
+++ heythere/apps/user/urls.py
@@ -1,3 +1,4 @@
+from dj_rest_auth.views import UserDetailsView
 from django.urls import path
 from fcm_django.api.rest_framework import FCMDeviceAuthorizedViewSet
 
@@ -6,5 +7,6 @@ app_name = "user"
 user_device_register_view = FCMDeviceAuthorizedViewSet.as_view({"post": "create"})
 
 urlpatterns = [
+    path("my/", UserDetailsView.as_view(), name="user_my"),
     path("device/register/", user_device_register_view, name="user_device_register"),
 ]
