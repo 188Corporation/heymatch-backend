cb721a376efac1d8bf2caec87601933cffc8f200
JinJis <david.jeong0724@gmail.com>
2022-09-13 22:35:34 +0900

    Add logout endpoint

--- heythere/apps/authen/urls.py
+++ heythere/apps/authen/urls.py
@@ -1,4 +1,4 @@
-from dj_rest_auth.views import LoginView
+from dj_rest_auth.views import LoginView, LogoutView
 from django.urls import path
 
 from .api.views import PhoneRegistrationViewSet, StreamTokenViewSet
@@ -20,6 +20,7 @@ urlpatterns = [
         LoginView.as_view(),
         name="phone-verification-code-authorize",
     ),
+    path("logout/", LogoutView.as_view(), name="rest_logout"),
     path(
         "stream/token/", stream_token_generate_view, name="stream-user-token-generate"
     ),
