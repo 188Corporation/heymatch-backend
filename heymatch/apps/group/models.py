754e78d813a2af58147c1b73a591ba0839b419aa
JinJis <david.jeong0724@gmail.com>
2022-11-23 22:33:38 +0900

    Higher mosaic for thumbnail

--- heymatch/apps/group/models.py
+++ heymatch/apps/group/models.py
@@ -175,7 +175,7 @@ class GroupProfileImage(OrderedModel):
 
     def process_thumbnail_blurred(self, image, filetype: str = "JPEG"):
         image.thumbnail((120, 120), Image.Resampling.LANCZOS)
-        image = image.filter(ImageFilter.BoxBlur(1))
+        image = image.filter(ImageFilter.BoxBlur(3))
         # Save thumbnail to in-memory file as StringIO
         temp_image = BytesIO()
         image.save(temp_image, filetype)
