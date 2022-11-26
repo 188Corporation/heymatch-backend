21a2e6f58cbbf5a8c76aff1d2b76ce96be419e7c
JinJis <david.jeong0724@gmail.com>
2022-11-27 00:17:47 +0900

    Make create_hotplace.py

---
+++ heymatch/infra/management/commands/create_hotplace.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+
+from heymatch.apps.hotplace.models import HotPlace
+from heymatch.apps.hotplace.tests.factories import (
+    RANDOM_HOTPLACE_INFO,
+    RANDOM_HOTPLACE_NAMES,
+)
+
+
+class Command(BaseCommand):
+    help = "Command for creating hotplace data"
+
+    def handle(self, *args, **options):
+        """Rest DB and migrate"""
+
+        for name in RANDOM_HOTPLACE_NAMES:
+            HotPlace.objects.get_or_create(
+                name=name,
+                zone_center_geoinfo=RANDOM_HOTPLACE_INFO[name]["zone_center_geoinfo"],
+                zone_boundary_geoinfos=RANDOM_HOTPLACE_INFO[name][
+                    "zone_boundary_geoinfos"
+                ],
+            )
+
+        self.stdout.write(self.style.SUCCESS("Successfully set up Hotplace!"))
