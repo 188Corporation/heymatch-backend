4339e4c52375cdb3c31df4a1e4bf950db1c2bc46
JinJis <david.jeong0724@gmail.com>
2022-12-09 03:21:50 +0900

    Change url pattern for chat webhook

--- heymatch/apps/chat/urls.py
+++ heymatch/apps/chat/urls.py
@@ -9,11 +9,11 @@ stream_chat_delete_viewset = StreamChatViewSet.as_view({"delete": "destroy"})
 stream_chat_webhook_viewset = StreamChatWebHookViewSet.as_view({"post": "hook"})
 
 urlpatterns = [
+    path("webhook/", stream_chat_webhook_viewset, name="stream-chat-webhook-view"),
     path("", stream_chat_list_viewset, name="stream-chat-list-view"),
     path(
         "<str:stream_cid>/",
         stream_chat_delete_viewset,
         name="stream-chat-exit-channel-view",
     ),
-    path("webhook/", stream_chat_webhook_viewset, name="stream-chat-webhook-view"),
 ]
