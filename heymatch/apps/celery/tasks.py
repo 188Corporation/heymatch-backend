e58726645d967a8e6bbcbf3046bba53a77da64b1
JinJis <david.jeong0724@gmail.com>
2023-08-10 23:55:14 +0900

    Revert back original logics

--- heymatch/apps/celery/tasks.py
+++ heymatch/apps/celery/tasks.py
@@ -241,7 +241,9 @@ def aggregate_business_report():
     female_krw_today = sum_up_payments(payment_today_by_female)
 
     # Top Hotplaces
-    top_hotplaces = Recent24HrTopGroupAddress.objects.all().first()
+    top_hotplaces = Recent24HrTopGroupAddress.objects.filter(
+        aggregated_at__gte=last_24hrs
+    ).first()
 
     slack_webhook.send(
         blocks=[
